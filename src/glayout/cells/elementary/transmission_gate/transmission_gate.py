dba69b3b2c19251265a429a7432a20285dee6623
Aleta Edna <168280856+aurxdeqo@users.noreply.github.com>
2026-04-17 17:16:36 +0700

    Add files via upload

--- src/glayout/cells/elementary/transmission_gate/transmission_gate.py
+++ src/glayout/cells/elementary/transmission_gate/transmission_gate.py
@@ -21,7 +21,8 @@ except ImportError:
     run_evaluation = None
 
 def add_tg_labels(tg_in: Component,
-                        pdk: MappedPDK
+                        pdk: MappedPDK,
+                        placement: str ="mirror"
                         ) -> Component:
 	
     tg_in.unlock()
@@ -29,36 +30,72 @@ def add_tg_labels(tg_in: Component,
     # list that will contain all port/comp info
     move_info = list()
     # create labels and append to info list
-    # vin
-    vinlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
-    vinlabel.add_label(text="VIN",layer=pdk.get_glayer("met2_label"))
-    move_info.append((vinlabel,tg_in.ports["N_multiplier_0_source_E"],None))
+    if placement == "mirror":
+        # vin
+        vinlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        vinlabel.add_label(text="VIN",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vinlabel,tg_in.ports["N_multiplier_0_source_E"],None))
+        
+        # vout
+        voutlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        voutlabel.add_label(text="VOUT",layer=pdk.get_glayer("met2_label"))
+        move_info.append((voutlabel,tg_in.ports["P_multiplier_0_drain_W"],None))
+        
+        # vcc
+        vcclabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+        vcclabel.add_label(text="VCC",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vcclabel,tg_in.ports["P_tie_S_top_met_S"],None))
+        
+        # vss
+        vsslabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+        vsslabel.add_label(text="VSS",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vsslabel,tg_in.ports["N_tie_S_top_met_N"], None))
+        
+        # VGP
+        vgplabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        vgplabel.add_label(text="VGP",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vgplabel,tg_in.ports["P_multiplier_0_gate_E"], None))
+        
+        # VGN
+        vgnlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        vgnlabel.add_label(text="VGN",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vgnlabel,tg_in.ports["N_multiplier_0_gate_E"], None))
+        
+    elif placement in ["vertical", "vertical_invert", "horizontal"]:
+        # VSS
+        vsslabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+        vsslabel.add_label(text="VSS",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vsslabel,tg_in.ports["N_tie_N_top_met_W"],None))
+        #gnd_ref = top_level << gndlabel;
     
-    # vout
-    voutlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
-    voutlabel.add_label(text="VOUT",layer=pdk.get_glayer("met2_label"))
-    move_info.append((voutlabel,tg_in.ports["P_multiplier_0_drain_W"],None))
+        #suply
+        vcclabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
+        vcclabel.add_label(text="VCC",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vcclabel,tg_in.ports["P_tie_N_top_met_W"],None))
+        #sup_ref = top_level << suplabel;
     
-    # vcc
-    vcclabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
-    vcclabel.add_label(text="VCC",layer=pdk.get_glayer("met2_label"))
-    move_info.append((vcclabel,tg_in.ports["P_tie_S_top_met_S"],None))
+        # output
+        voutlabel = rectangle(layer=pdk.get_glayer("met3_pin"),size=(0.27,0.27),centered=True).copy()
+        voutlabel.add_label(text="VOUT",layer=pdk.get_glayer("met3_label"))
+        move_info.append((voutlabel,tg_in.ports["P_drain_top_met_N"],None))
+        #op_ref = top_level << outputlabel;
     
-    # vss
-    vsslabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.5,0.5),centered=True).copy()
-    vsslabel.add_label(text="VSS",layer=pdk.get_glayer("met2_label"))
-    move_info.append((vsslabel,tg_in.ports["N_tie_S_top_met_N"], None))
+        # input
+        vinlabel = rectangle(layer=pdk.get_glayer("met3_pin"),size=(0.27,0.27),centered=True).copy()
+        vinlabel.add_label(text="VIN",layer=pdk.get_glayer("met3_label"))
+        move_info.append((vinlabel,tg_in.ports["P_source_top_met_N"], None))
+        #ip_ref = top_level << inputlabel;
     
-    # VGP
-    vgplabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
-    vgplabel.add_label(text="VGP",layer=pdk.get_glayer("met2_label"))
-    move_info.append((vgplabel,tg_in.ports["P_multiplier_0_gate_E"], None))
+        # VGN
+        vgnlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        vgnlabel.add_label(text="VGN",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vgnlabel,tg_in.ports["N_gate_S"], None))
     
-    # VGN
-    vgnlabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
-    vgnlabel.add_label(text="VGN",layer=pdk.get_glayer("met2_label"))
-    move_info.append((vgnlabel,tg_in.ports["N_multiplier_0_gate_E"], None))
-
+        # VGP
+        vgplabel = rectangle(layer=pdk.get_glayer("met2_pin"),size=(0.27,0.27),centered=True).copy()
+        vgplabel.add_label(text="VGP",layer=pdk.get_glayer("met2_label"))
+        move_info.append((vgplabel,tg_in.ports["P_gate_N"], None))
+    
     # move everything to position
     for comp, prt, alignment in move_info:
         alignment = ('c','b') if alignment is None else alignment
@@ -88,47 +125,78 @@ def get_component_netlist(component):
     # Fallback: return the string representation (should not happen in normal operation)
     return component.info.get('netlist', '')
 
-def sky130_add_tg_labels(tg_in: Component) -> Component:
+def sky130_add_tg_labels(tg_in: Component, placement: str= "mirror") -> Component:
 	
     tg_in.unlock()
     
     # define layers`
     met1_pin = (68,16)
+    met2_pin = (69,16)
     met1_label = (68,5)
+    met2_label = (69,5)
     li1_pin = (67,16)
     li1_label = (67,5)
     # list that will contain all port/comp info
     move_info = list()
     # create labels and append to info list
-    # vin
-    vinlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
-    vinlabel.add_label(text="VIN",layer=met1_label)
-    move_info.append((vinlabel,tg_in.ports["N_multiplier_0_source_E"],None))
-    
-    # vout
-    voutlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
-    voutlabel.add_label(text="VOUT",layer=met1_label)
-    move_info.append((voutlabel,tg_in.ports["P_multiplier_0_drain_W"],None))
-    
-    # vcc
-    vcclabel = rectangle(layer=met1_pin,size=(0.5,0.5),centered=True).copy()
-    vcclabel.add_label(text="VCC",layer=met1_label)
-    move_info.append((vcclabel,tg_in.ports["P_tie_S_top_met_S"],None))
-    
-    # vss
-    vsslabel = rectangle(layer=met1_pin,size=(0.5,0.5),centered=True).copy()
-    vsslabel.add_label(text="VSS",layer=met1_label)
-    move_info.append((vsslabel,tg_in.ports["N_tie_S_top_met_N"], None))
-    
-    # VGP
-    vgplabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
-    vgplabel.add_label(text="VGP",layer=met1_label)
-    move_info.append((vgplabel,tg_in.ports["P_multiplier_0_gate_E"], None))
-    
-    # VGN
-    vgnlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
-    vgnlabel.add_label(text="VGN",layer=met1_label)
-    move_info.append((vgnlabel,tg_in.ports["N_multiplier_0_gate_E"], None))
+    if placement == "mirror":
+        # vin
+        vinlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
+        vinlabel.add_label(text="VIN",layer=met1_label)
+        move_info.append((vinlabel,tg_in.ports["N_multiplier_0_source_E"],None))
+        
+        # vout
+        voutlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
+        voutlabel.add_label(text="VOUT",layer=met1_label)
+        move_info.append((voutlabel,tg_in.ports["P_multiplier_0_drain_W"],None))
+        
+        # vcc
+        vcclabel = rectangle(layer=met1_pin,size=(0.5,0.5),centered=True).copy()
+        vcclabel.add_label(text="VCC",layer=met1_label)
+        move_info.append((vcclabel,tg_in.ports["P_tie_S_top_met_S"],None))
+        
+        # vss
+        vsslabel = rectangle(layer=met1_pin,size=(0.5,0.5),centered=True).copy()
+        vsslabel.add_label(text="VSS",layer=met1_label)
+        move_info.append((vsslabel,tg_in.ports["N_tie_S_top_met_N"], None))
+        
+        # VGP
+        vgplabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
+        vgplabel.add_label(text="VGP",layer=met1_label)
+        move_info.append((vgplabel,tg_in.ports["P_multiplier_0_gate_E"], None))
+        
+        # VGN
+        vgnlabel = rectangle(layer=met1_pin,size=(0.27,0.27),centered=True).copy()
+        vgnlabel.add_label(text="VGN",layer=met1_label)
+        move_info.append((vgnlabel,tg_in.ports["N_multiplier_0_gate_E"], None))
+
+    elif placement in ["vertical", "vertical_invert", "horizontal"]:
+        # VIN & VOUT
+        vin_label = rectangle(layer=met2_pin, size=(0.27,0.27), centered=True).copy()
+        vin_label.add_label(text="VIN", layer=met2_label)
+        move_info.append((vin_label, tg_in.ports["P_source_top_met_N"], None))
+
+        vout_label = rectangle(layer=met2_pin, size=(0.27,0.27), centered=True).copy()
+        vout_label.add_label(text="VOUT", layer=met2_label)
+        move_info.append((vout_label, tg_in.ports["P_drain_top_met_N"], None))
+
+        # VDD (VCC) & VSS
+        vcc_label = rectangle(layer=met1_pin, size=(0.5,0.5), centered=True).copy()
+        vcc_label.add_label(text="VCC", layer=met1_label)
+        move_info.append((vcc_label, tg_in.ports["P_tie_N_top_met_W"], None))
+
+        vss_label = rectangle(layer=met1_pin, size=(0.5,0.5), centered=True).copy()
+        vss_label.add_label(text="VSS", layer=met1_label)
+        move_info.append((vss_label, tg_in.ports["N_tie_N_top_met_W"], None))
+
+        # CLK & CLK_INV (Nama berubah untuk Chopper)
+        vgnlabel = rectangle(layer=met1_pin, size=(0.27,0.27), centered=True).copy()
+        vgnlabel.add_label(text="VGN", layer=met1_label)
+        move_info.append((vgnlabel, tg_in.ports["N_gate_S"], None))
+
+        vgplabel = rectangle(layer=met1_pin, size=(0.27,0.27), centered=True).copy()
+        vgplabel.add_label(text="VGP", layer=met1_label)
+        move_info.append((vgplabel, tg_in.ports["P_gate_N"], None))
 
     # move everything to position
     for comp, prt, alignment in move_info:
@@ -149,15 +217,19 @@ def tg_netlist(nfet: Component, pfet: Component) -> Netlist:
 
          return netlist
 
-@cell
 def  transmission_gate(
         pdk: MappedPDK,
+        placement: str = "mirror",
         width: tuple[float,float] = (1,1),
         length: tuple[float,float] = (None,None),
         fingers: tuple[int,int] = (1,1),
         multipliers: tuple[int,int] = (1,1),
         substrate_tap: bool = False,
-        tie_layers: tuple[str,str] = ("met2","met1"),
+        dummy_1: tuple[bool,bool] = (True,True),
+        dummy_2: tuple[bool,bool] = (True,True),
+        tie_layers1: tuple[str,str] = ("met2","met1"),
+        tie_layers2: tuple[str,str] = ("met2","met1"),
+        sd_rmult: int=1,
         **kwargs
         ) -> Component:
     """
@@ -170,18 +242,72 @@ def  transmission_gate(
     top_level = Component(name="transmission_gate")
 
     #two fets
-    nfet = nmos(pdk, width=width[0], fingers=fingers[0], multipliers=multipliers[0], with_dummy=True, with_dnwell=False,  with_substrate_tap=False, length=length[0], **kwargs)
-    pfet = pmos(pdk, width=width[1], fingers=fingers[1], multipliers=multipliers[1], with_dummy=True, with_substrate_tap=False, length=length[1], **kwargs)
+    nfet = nmos(pdk, width=width[0], fingers=fingers[0], multipliers=multipliers[0], with_dummy=dummy_1, with_dnwell=False,  with_substrate_tap=False, length=length[0], tie_layers=tie_layers1, sd_rmult=sd_rmult, **kwargs)
+    pfet = pmos(pdk, width=width[1], fingers=fingers[1], multipliers=multipliers[1], with_dummy=dummy_2, with_substrate_tap=False, length=length[1], tie_layers=tie_layers2, sd_rmult=sd_rmult, **kwargs)
+    
     nfet_ref = top_level << nfet
     pfet_ref = top_level << pfet 
-    pfet_ref = rename_ports_by_orientation(pfet_ref.mirror_y())
-
-    #Relative move
-    pfet_ref.movey(nfet_ref.ymax + evaluate_bbox(pfet_ref)[1]/2 + pdk.util_max_metal_seperation())
+    viam2m3 = via_stack(pdk, "met2", "met3", centered=True)
+    ref_dimensions = evaluate_bbox(nfet)
     
-    #Routing
-    top_level << c_route(pdk, nfet_ref.ports["multiplier_0_source_E"], pfet_ref.ports["multiplier_0_source_E"])
-    top_level << c_route(pdk, nfet_ref.ports["multiplier_0_drain_W"], pfet_ref.ports["multiplier_0_drain_W"], viaoffset=False)
+    if placement == "mirror":
+        pfet_ref = rename_ports_by_orientation(pfet_ref.mirror_y())
+        #Relative move
+        pfet_ref.movey(nfet_ref.ymax + evaluate_bbox(pfet_ref)[1]/2 + pdk.util_max_metal_seperation())
+        
+        #Routing
+        top_level << c_route(pdk, nfet_ref.ports["multiplier_0_source_E"], pfet_ref.ports["multiplier_0_source_E"])
+        top_level << c_route(pdk, nfet_ref.ports["multiplier_0_drain_W"], pfet_ref.ports["multiplier_0_drain_W"], viaoffset=False)
+        
+    elif placement == "vertical":
+        nfet_ref.mirror_y()
+        nfet_ref.movey(pfet_ref.ymin - ref_dimensions[1]/2 - pdk.util_max_metal_seperation()-0.5)
+        
+        #via for input and output
+        drain_P_via = top_level << viam2m3
+        source_P_via = top_level << viam2m3
+
+        drain_P_via.move(pfet_ref.ports["multiplier_0_source_W"].center).movex(-0.75).movey((nfet_ref.ymin-nfet_ref.ymax)/1.7)
+        source_P_via.move(pfet_ref.ports["multiplier_0_drain_E"].center).movex(0.75).movey((nfet_ref.ymin-nfet_ref.ymax)/1.7)
+        top_level << c_route(pdk, pfet_ref.ports["multiplier_0_source_W"], nfet_ref.ports["multiplier_0_source_W"])
+        top_level << c_route(pdk, pfet_ref.ports["multiplier_0_drain_E"], nfet_ref.ports["multiplier_0_drain_E"])
+
+        top_level.add_ports(drain_P_via.get_ports_list(), prefix="P_drain_")
+        top_level.add_ports(source_P_via.get_ports_list(), prefix="P_source_")
+        
+    elif placement == "vertical_invert":
+        pfet_ref.mirror_y()
+        pfet_ref.movey(nfet_ref.ymin - ref_dimensions[1]/2 - pdk.util_max_metal_seperation()-0.5)
+
+        #via for input and output
+        drain_P_via = top_level << viam2m3
+        source_P_via = top_level << viam2m3
+
+        drain_P_via.move(nfet_ref.ports["multiplier_0_source_W"].center).movex(-0.75).movey((pfet_ref.ymin-pfet_ref.ymax)/1.7)
+        source_P_via.move(nfet_ref.ports["multiplier_0_drain_E"].center).movex(0.75).movey((pfet_ref.ymin-pfet_ref.ymax)/1.7)
+        top_level << c_route(pdk, pfet_ref.ports["multiplier_0_source_W"], nfet_ref.ports["multiplier_0_source_W"])
+        top_level << c_route(pdk, pfet_ref.ports["multiplier_0_drain_E"], nfet_ref.ports["multiplier_0_drain_E"])
+
+        top_level.add_ports(drain_P_via.get_ports_list(), prefix="P_drain_")
+        top_level.add_ports(source_P_via.get_ports_list(), prefix="P_source_")
+        
+    elif placement == "horizontal":
+        nfet_ref.movex(pfet_ref.xmax + ref_dimensions[0]/2 + pdk.util_max_metal_seperation()+1)
+ 
+        #via for input and output
+        drain_P_via = top_level << viam2m3
+        source_P_via = top_level << viam2m3
+        
+        source_P_via.move(pfet_ref.ports["multiplier_0_source_W"].center).movex(ref_dimensions[0]*0.9)
+        drain_P_via.move(pfet_ref.ports["multiplier_0_drain_E"].center).movex(ref_dimensions[0]*0.15)
+        top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_source_E"], nfet_ref.ports["multiplier_0_source_W"])
+        top_level << straight_route(pdk, pfet_ref.ports["multiplier_0_drain_E"], nfet_ref.ports["multiplier_0_drain_W"])
+
+        top_level.add_ports(drain_P_via.get_ports_list(), prefix="P_drain_")
+        top_level.add_ports(source_P_via.get_ports_list(), prefix="P_source_")
+        
+    else:
+            raise ValueError("Placement must be either 'mirror', 'vertical', 'vertical _inv', or 'horizontal'")
     
     #Renaming Ports
     top_level.add_ports(nfet_ref.get_ports_list(), prefix="N_")
@@ -216,6 +342,7 @@ def  transmission_gate(
 
 
     return component
+    
 if __name__ == "__main__":
     # OLD EVAL CODE
     # comp = transmission_gate(sky130)
